dabae782a7d6c9ff290801cf5d50a40a3d5e49b7
takefumi mizumoto <mizumoto.takefumi@gmail.com>
2020-03-30 13:58:05 +0900

    Add index.html.erb

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -3,4 +3,21 @@ class PropertiesController < ApplicationController
   def new
     @property = Property.new
   end
+
+  def create
+    property = Property.new(property_params)
+    property.save
+    redirect_to properties_url, notice: "物件「#{property.name}」を登録しました。"
+  end
+
+  def index
+    @properties = Property.all
+  end
+
+  private
+
+  def property_params
+    params.require(:property).permit(:name, :rent, :address, :age, :remarks)
+  end
+
 end
